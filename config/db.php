7e059b1059af50b0600cfd036d8344a4b12ed3ae
Aessarrajabadi <aessarrajabadi10@gmail.com>
2024-10-08 13:20:21 +0530

    hy
    -

---
+++ config/db.php
@@ -0,0 +1,8 @@
+
+    <?php
+        define('db_host', 'localhost');
+        define('db_user', 'admin');
+        define('db_pass', 'Asrbadi@123');
+        define('db_name', 'Asrbadi');
+    ?>
+    
